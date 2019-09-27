64840d6408c7df93f9bcbfe4f25382a69aae13fb
Kasio <kasioeduardo13@gmail.com>
2019-09-27 10:30:18 -0300

    Estrutura organizada

---
+++ src/styles/global.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,700&display=swap');
+
+  *{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+  }
+
+  html,body,#root{
+    height:100%;
+  }
+
+  body{
+    text-rendering:optimizeLegibility;
+    -webkit-font-smoothing:antialiased;
+    background:#181818;
+    font-family: 'Montserrat', sans-serif;
+    color:#fff;
+  }
+`;
